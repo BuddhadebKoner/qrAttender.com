41889062bd78e8cca76af6b9ef0586b4bac45f1c
BuddhadebKoner <jeetkoner36@gmail.com>
2024-10-04 23:23:59 +0530

    making new thing

---
+++ src/App.tsx
@@ -0,0 +1,11 @@
+import { GoogleGeminiEffect } from "./components/ui/google-gemini-effect"
+
+const App = () => {
+  return (
+    <div className="w-full h-full flex flex-1 justify-center items-center">
+      <GoogleGeminiEffect pathLengths={[]} /> 
+    </div>
+  )
+}
+
+export default App
